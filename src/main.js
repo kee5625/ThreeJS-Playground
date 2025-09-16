37e0bb6195f5c01981ddabf7724034f0675804ee
Karthik R <karthikeya.rachamolla@gmail.com>
2025-09-16 09:15:43 -0400

    finished adding a cube

--- src/main.js
+++ src/main.js
@@ -10,17 +10,28 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const edges = new THREE.EdgesGeometry( geometry);
+const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0xffffff}));
+
 const material = new THREE.MeshBasicMaterial( {color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+scene.add(line);
 
 camera.position.z = 5;
 
 function animate() {
+  cube.rotation.x += 0.01;
+  line.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  line.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
+
+
 
 
 /*
